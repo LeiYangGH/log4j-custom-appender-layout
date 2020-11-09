b24c1a7d0bce7f91f6dc62736d95c0ccafac8c52
LeiYangGH <675686066@qq.com>
2020-11-09 12:02:54 +0800

    extends AppenderSkeleton

--- src/main/java/assign251_2/MemAppender.java
+++ src/main/java/assign251_2/MemAppender.java
@@ -1,5 +1,22 @@
 package assign251_2;
 
-public class MemAppender {
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
 
+public class MemAppender extends AppenderSkeleton {
+
+  @Override
+  protected void append(LoggingEvent loggingEvent) {
+
+  }
+
+  @Override
+  public void close() {
+
+  }
+
+  @Override
+  public boolean requiresLayout() {
+    return false;
+  }
 }
