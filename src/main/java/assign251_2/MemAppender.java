79690aff74efcd198e46ce2a927679683b4315b4
LeiYangGH <675686066@qq.com>
2020-11-11 23:40:58 +0800

    3 methods

--- src/main/java/assign251_2/MemAppender.java
+++ src/main/java/assign251_2/MemAppender.java
@@ -3,8 +3,15 @@ package assign251_2;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.spi.LoggingEvent;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+
 public class MemAppender extends AppenderSkeleton {
     private static MemAppender uniqueInstance;
+    private CopyOnWriteArrayList<LoggingEvent> lstEvents = new CopyOnWriteArrayList<>();
 
     private MemAppender() {
     }
@@ -22,7 +29,7 @@ public class MemAppender extends AppenderSkeleton {
 
     @Override
     protected void append(LoggingEvent loggingEvent) {
-
+        this.lstEvents.add(loggingEvent);
     }
 
     @Override
@@ -34,4 +41,21 @@ public class MemAppender extends AppenderSkeleton {
     public boolean requiresLayout() {
         return false;
     }
+
+    public List<LoggingEvent> getCurrentLogs() {
+        return Collections.unmodifiableList(this.lstEvents);
+    }
+
+    public List<String> getEventStrings() {
+        return Collections.unmodifiableList(this.getCurrentLogs().stream().map(x ->
+                x.getRenderedMessage()).collect(Collectors.toList()));
+    }
+
+    public void printLogs() {
+        for (String s : getEventStrings()) {
+            System.out.println(s);
+        }
+        this.lstEvents.clear();
+    }
+
 }
