272492f1760bebce1479ede38d240c44a02103d6
LeiYangGH <675686066@qq.com>
2020-11-11 23:26:59 +0800

    singleton

--- src/main/java/assign251_2/MemAppender.java
+++ src/main/java/assign251_2/MemAppender.java
@@ -4,19 +4,34 @@ import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.spi.LoggingEvent;
 
 public class MemAppender extends AppenderSkeleton {
+    private static MemAppender uniqueInstance;
 
-  @Override
-  protected void append(LoggingEvent loggingEvent) {
+    private MemAppender() {
+    }
 
-  }
+    public static MemAppender getInstance() {
+        if (uniqueInstance == null) {
+            synchronized (MemAppender.class) {
+                // check again to avoid multi-thread access
+                if (uniqueInstance == null)
+                    uniqueInstance = new MemAppender();
+            }
+        }
+        return uniqueInstance;
+    }
 
-  @Override
-  public void close() {
+    @Override
+    protected void append(LoggingEvent loggingEvent) {
 
-  }
+    }
 
-  @Override
-  public boolean requiresLayout() {
-    return false;
-  }
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public boolean requiresLayout() {
+        return false;
+    }
 }
