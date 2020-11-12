8ec8548a28aec9f3b40469af606aafda4cb5a598
LeiYangGH <675686066@qq.com>
2020-11-12 12:26:29 +0800

    add Main to use log

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello maven");
-    }
+  protected static final Logger logger = LogManager.getLogger(Main.class);
+
+  public static void main(String[] args) {
+    logger.info("this is some log");
+    System.out.println("Hello maven");
+  }
 }
